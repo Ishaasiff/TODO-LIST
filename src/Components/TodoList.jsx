565ec530a0154f40f6489efeec9f4a26540d3102
Ishaasif <https://github.com/Ishaasiff/Portfolio>
2025-11-22 15:46:03 +0500

    Normalize line endings

---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import './TodoList.css';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      try{
+        setTodos(JSON.parse(savedTodos))
+      }
+      catch{
+        localStorage.removeItem("todos");
+      }
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const handleAddTodo = () => {
+    if (headingInput.trim() === '') return;
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+  };
+  const handleAddList = (index) => {
+    if (listInputs[index] && listInputs[index].trim() !== '') {
+      const newTodos = [...todos];
+      newTodos[index].lists.push(listInputs[index]);
+      setTodos(newTodos);
+      setListInputs({ ...listInputs, [index]: '' });
+    }
+  };
+  const handleListInputChange = (index, value) => {
+    setListInputs({ ...listInputs, [index]: value });
+  };
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  const handleDeleteList = (todoIndex, listIndex) => {
+    setTodos((prev) =>
+      prev.map((todo, i) =>
+        i === todoIndex
+          ? { ...todo, lists: todo.lists.filter((_, j) => j !== listIndex) }
+          : todo
+      )
+    );
+  };
+  return (
+    <>
+      <div className='wrapper'>
+        <div className="todo-container">
+          <h1 className="title">My Todo List</h1>
+          <div className="input-container">
+            <input
+              type="text"
+              className="heading-input"
+              placeholder="Enter heading"
+              value={headingInput}
+              onChange={(e) => { setHeadingInput(e.target.value); }}
+            />
+            <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
+          </div>
+        </div>
+        <div className="todo_main">
+          {todos.map((todo, index) => (
+            <div key={index} className="todo-card">
+              <div className="heading_todo">
+                <h3>{todo.heading}</h3>
+                <button className="delete-button-heading" onClick={handleDeleteTodo}>Delete Heading</button>
+              </div>
+              <ul>
+                {todo.lists.map((list, listIndex) => (
+                  <li key={listIndex} className='todo_inside_list'>
+                    <p>{list}</p> 
+                    <button className="delete-button-heading" onClick={() => handleDeleteList(index,listIndex)}>Delete List</button>
+                  </li>
+                ))}
+              </ul>
+              
+              <div className='add_list'><input
+                type="text"
+                className="list-input"
+                placeholder="Add List"
+                value={listInputs[index] || ''}
+                onChange={(e) => handleListInputChange(index, e.target.value)} />
+                <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TodoList;  
